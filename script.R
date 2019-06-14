3245fb5c8be87060426e6c2c5f2b7a110f58f5d7
josh-larock <51710718+josh-larock@users.noreply.github.com>
2019-06-14 19:33:59 -0400

    - plotted distributions - subsetted the data to Entire Home + Manhattan

--- script.R
+++ script.R
@@ -12,7 +12,11 @@ library(caretEnsemble)
 library(doParallel)
 
 #Import AirBnB File
-raw_data = read.csv("http://data.insideairbnb.com/united-states/ny/new-york-city/2019-06-02/visualisations/listings.csv", header = TRUE)
+
+# To Publish
+
+#raw_data = read.csv("http://data.insideairbnb.com/united-states/ny/new-york-city/2019-06-02/visualisations/listings.csv", header = TRUE)
+raw_data = read.csv(file="c:/Users/joshl/Desktop/rdata/nyc_airbnb_june1.csv", header = TRUE)
 
 # Get an initial feel for the dataset
 glimpse(raw_data)
@@ -27,11 +31,25 @@ colnames(raw_data)[names(raw_data) == "neighbourhood_group"] = "borough"
 # The reviews_per_month column has NAs when there hasn't been any reviews, we'll replace these with 0
 raw_data$reviews_per_month[is.na(raw_data$reviews_per_month)] = 0
 
-### Let's make a new variable, the dataset without the factors and check out some correlations
-### And distributions
+# Confirm there are any NAs left in the data
+anyNA(raw_data)
 
-# Create a new variable listings_num_only 
-num_only = subset(raw_data, select = -c(2, 4, 5, 6, 9, 13))
-cor_num_only = cor(num_only)
-corrplot(cor_num_only, method = "square", type = "lower")
+
+# Let's make a new variable, the dataset without the factors and check out correlations & distributions
+ggplot(data = raw_data) +
+  facet_grid(room_type ~ borough) +
+  geom_histogram(mapping = aes(price), bins = 30)
+
+# Let's make a new variable, the dataset without the factors and check out correlations & distributions
+
+# Crazy expesive outliers we're going to ignore 
+# I want to focus on Entire home/apt in Manhattan
+data = subset(raw_data, price < 1250 & borough == "Manhattan" & room_type == "Entire home/apt")
+
+ggplot(data = data) +
+  geom_histogram(mapping = aes(price), bins = 50)
+
+# Check for correlations
+corrplot(cor(data[, c(-2, -4, -5, -6, -9, -13)]), method = "square")
+# Nothing Meaningful
 
