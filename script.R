60efcd24842fc95cca6c68c9bf58b60995f472ae
josh-larock <51710718+josh-larock@users.noreply.github.com>
2019-06-17 21:10:09 -0400

    Found new dataset with many more factors, but also much work cleaning work to do.

--- script.R
+++ script.R
@@ -1,5 +1,3 @@
-# Import
-
 ### Import Libraries
 library(dplyr)
 library(ggplot2)
@@ -14,60 +12,56 @@ library(caretEnsemble)
 library(doParallel)
 library(tmaptools)
 library(viridis)
+library(data.table)
 
-### Import AirBnB file ### raw_data = read.csv("http://data.insideairbnb.com/united-states/ny/new-york-city/2019-06-02/visualisations/listings.csv", header = TRUE)
-raw.data <- read.csv(file="c:/Users/joshl/Desktop/rdata/nyc_airbnb_june1.csv", header = TRUE)
+### Import AirBnB file ### 
+#raw.data <- fread("http://data.insideairbnb.com/united-states/ny/new-york-city/2019-06-02/data/listings.csv.gz", header = TRUE)
+#raw.data <- read.csv(file="c:/Users/joshl/Desktop/rdata/nyc_airbnb_june1.csv", header = TRUE)
 
+### Check Dimension of Data
+dim(raw.data)
+glimpse(raw.data)
 # Data Cleaning
 
-### View data
-glimpse(raw.data)
-head(raw.data)
-str(raw.data)
+### Check how many NAs are in each column
+na.count <- sapply(raw.data, function(y) sum(length(which(is.na(y)))))
+na.count[na.count > 0]
 
-### Change neighbourhood_group column name to borough
-colnames(raw.data)[names(raw.data) == "neighbourhood_group"] = "borough"
+### Check how many empty strings are in each column
+blank.count <- sapply(raw.data, function(y) sum(length(which(y == ""))))
+blank.count[blank.count > 0]
 
-### The reviews_per_month column has NAs when there hasn't been any reviews, we'll replace these with 0
-raw.data$reviews_per_month[is.na(raw.data$reviews_per_month)] = 0
+### Remove columns with a ton of NAs
+data <- subset(raw.data, select = c(host_is_superhost, neighbourhood_group_cleansed,
+                                    neighbourhood_cleansed, latitude, longitude, property_type,
+                                    room_type, accommodates, bathrooms, beds, bed_type, price,
+                                    cleaning_fee, guests_included, extra_people, minimum_nights,
+                                    number_of_reviews, review_scores_rating, review_scores_accuracy,
+                                    review_scores_cleanliness, review_scores_checkin,
+                                    review_scores_communication, review_scores_location,
+                                    review_scores_value, calculated_host_listings_count))
 
-### Confirm there are any NAs left in the data
-anyNA(raw.data)
+### Clear any entries with NAs left
+data <- data[complete.cases(data)]
 
-# Subset Data
-
-### Subset Entire home/apt in Manhattan that are between 100-400$ a night
-data <- subset(raw.data, price < 300 & borough == "Manhattan" & room_type == "Entire home/apt")
-
-### View distribution of response variable
-ggplot(data = data) +
-  geom_histogram(mapping = aes(price), bins = 16)
+blank.count <- sapply(data, function(y) sum(length(which(y == ""))))
+blank.count[blank.count > 0]
+### Find and remove columns that are not mostly full
+glimpse(data)
 
 # Spatial Visualization
 
 ### Get a map of Manhattan for visualizations
-map <- ggmap(get_stamenmap(rbind(as.numeric(paste(geocode_OSM("Manhattan")$bbox))), zoom = 12))
+map <- ggmap(get_stamenmap(rbind(as.numeric(paste(geocode_OSM("New York City")$bbox))), zoom = 13))
 
 ### plot the data
-map + geom_point(data = data, aes(x = longitude, y =latitude, colour = factor(neighbourhood)),
-             alpha = 0.1) + guides(colour = guide_legend(override.aes = list(alpha = 1))) +
-  theme(legend.title = element_text(colour="chocolate", size=16, face="bold"))
+#map + geom_point(data = data, aes(x = longitude, y =latitude, colour = factor(neighbourhood)),
+#                 alpha = 0.1) + guides(colour = guide_legend(override.aes = list(alpha = 1))) +
+# theme(legend.title = element_text(colour="chocolate", size=16, face="bold"))
 
 ### Density map
 map + stat_density2d(mapping = aes(x = longitude, y =latitude, fill = ..level.., 
-                               alpha = ..level..), 
-                 size = 0.01, bins = 30,geom = "polygon", data = data) + 
+                                   alpha = ..level..), 
+                     size = 0.01, bins = 30,geom = "polygon", data = data) + 
   scale_fill_gradient(low = "dark blue", high = "orange") +
   scale_alpha(range = c(0.05, 0.3), guide = FALSE)
-
-# Analyse distrubutions and correlations 
-
-### Create a new data set with just numerical factors
-num.data <- data[, c(-2, -4, -5, -6, -9, -13)]
-
-### Check correlations
-corrplot(cor(num.data), method = "square")
-
-### Check VIF
-simple.lm <- lm(price ~ ., data = num.data)
-vif(simple.lm)
